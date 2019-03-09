5c6f71b9d8eff07ba6c6424cb2e926a6f70f2918
mydraal56 <mydraal56@hotmail.com>
2019-03-09 18:30:47 -0500

    Getting started.
    Getting started.

---
+++ TimeCards4Kids/TimeCards4Kids/Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeCards4Kids.Models
+{
+    public class ApplicationUser 
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public int relativeID { get; set; }
+        public bool child { get; set; }
+
+    }
+}
