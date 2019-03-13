d00f60fd2fda29c92e7ca8f179e38842bdb7b655
mydraal56 <mydraal56@hotmail.com>
2019-03-13 08:40:34 -0400

    New project
    User roles in identity

---
+++ TimeCards4Kids/TimeCards4Kids/Contexts/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TimeCards4Kids.Contexts
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+    }
+
+    public DbSet<UserLogin> UserLogins { get; set; }
+}
---
+++ TimeCards4Kids/TimeCards4Kids/Models/UserLogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace TimeCards4Kids.Models
+{
+    public class UserLogin
+    {
+        [Key]
+        public int userId { get; set; }
+        public string distinguishedUserName { get; set; }
+        public string passwordHash { get; set; }
+    }
+}
